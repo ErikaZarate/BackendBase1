68119837276177038cce9e9fb7b5d17653c6e1e3
Lopez Delgado Jose Andres <isc17350296@gmail.com>
2022-09-28 10:00:35 -0500

    Status Errores

--- Controllers/messages.js
+++ Controllers/messages.js
@@ -1,21 +1,22 @@
 const { request, response } = require("express")
 const rootMessage=(req=request,res=response) =>{
-    res.json({msg:'Hola'})
+    res.status(404).json({msg:'Hola'})
+
 }
 const hiMessage = (req=request,res= response) =>{
-    res.json({msg:'Hola Mundo'})    
+    res.status(405).json({msg:'Hola Mundo'})    
 }
 const byeMessage = (req=request,res=response) =>{
-    res.json({msg:'Adiós mundo'}) 
+    res.status(406).json({msg:'Adiós mundo'}) 
 }
 const postMessage = (req=request,res= response) =>{
-    res.json({msg:'Mensaje POST'}) 
+    res.status(407).json({msg:'Mensaje POST'}) 
 }
 const putMessage = (req=request,res=response) =>{
-    res.json({msg:'Mensaje POST'})
+    res.status(408).json({msg:'Mensaje POST'})
 }
 const deleteMessage = (req=request,res=response) =>{
-    res.json({msg:'Mensaje DELETE '}) 
+    res.status(409).json({msg:'Mensaje DELETE '}) 
 }
 module.exports = {
     rootMessage,
