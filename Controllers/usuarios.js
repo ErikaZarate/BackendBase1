6504ae03132efefa9d979370f291f6af160d969f
Erika Zarate <erikazg20@gmail.com>
2022-10-14 10:56:29 -0500

    consultas

--- Controllers/usuarios.js
+++ Controllers/usuarios.js
@@ -1,6 +1,26 @@
 const { request, response } = require("express")
-const getUsers= (req = request,res = response) => {
-    console.log("Funcion getUsers")
-    res.json({msg:"Funcion getUsers"})
+const pool=require("../db/connection")
+const getUsers=async(req = request,res = response) => {
+   
+    let conn;
+
+
+    try{
+        conn=await pool.getConnection()
+        const users=await conn.query("SELECT * FROM Usuarios",(error)=>{throw  new error})
+
+        if(!users){
+            res.status(404).json({msg:"no se encontraon registros"})
+            return
+        }
+        res.json({users})
+    }catch(error){
+        console.log(error)
+        res.status(500).json({error})
+    }finally{
+        if(conn){
+            conn.end()
+        }
+    }
 }
 module.exports={getUsers}
--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
 const mariadb = require("mariadb")
 const config ={
     host: process.env.DB_HOST,
-    user: process.env.DBUSER,
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
