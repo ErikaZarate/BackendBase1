874d126343726a857239c1d766973a2c91f258e1
Erika Zarate <erikazg20@gmail.com>
2022-10-28 10:21:55 -0500

    Modificacion

--- Controllers/usuarios.js
+++ Controllers/usuarios.js
@@ -135,5 +135,62 @@ INSERT INTO Usuarios (
          }
      }
  }
-module.exports={getUsers,getUserByID,deleteUserByID,addUser}
+
+ const updateUserByUsuario=async(req = request,res = response) => {
+    const{
+        Nombre,
+        Apellidos,
+        Edad,
+        Genero,
+        Usuario,
+        Contrasena, 
+        Fecha_Nacimiento="1900-01.01"
+
+    }=req.body
+    if(
+        !Nombre||
+        !Apellidos||
+        !Edad||
+        
+        !Contrasena
+        
+    ){
+        res.status(400).json({msg:"Falta información del usuario" })
+    return
+    }
+ let conn;
+     try{
+         conn=await pool.getConnection()
+
+         const [user]=await conn.query(`SELECT Usuario, Nombre, Apellidos, Edad, Genero,Fecha_Nacimiento
+         FROM usuarios WHERE Usuario= '${Usuario}'`)
+         if (!user){
+            res.status(403).json({msg:`El usuario ${Usuario} se actualizo satisfactoriamente`})
+         }
+         const {affectedRows}=await conn.query(`
+UPDATE Usuarios SET
+        Nombre='${Nombre || user.Nombre  }',
+        Apellidos='${Apellidos || user.Apellidos} ',
+        Edad='${Edad || user.Edad}',
+        Genero, '${Genero|| user.Genero}'
+        Fecha_Nacimiento=  '${Fecha_Nacimiento}',
+      
+        WHERE Usuario='${Usuario}'
+        `,(error)=>{throw new error})
+ 
+         if(affectedRows===0){
+             res.status(404).json({msg:`no se pudo actualizar el registro del usuario${Usuario}`})
+             return
+         }
+         res.json({msg:`El usuario con ${Usuario}se agrago satisfactoriamente `})
+     }catch(error){
+         console.log(error)
+         res.status(500).json({error})
+     }finally{
+         if(conn){
+             conn.end()
+         }
+     }
+ }
+module.exports={getUsers,getUserByID,deleteUserByID,addUser,updateUserByUsuario}
 
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,9 +1,10 @@
 const {Router}=require("express")
-const {getUsers, getUserByID,deleteUserByID,addUser} =require("../Controllers/usuarios")
+const {getUsers, getUserByID,deleteUserByID,addUser,updateUserByUsuario} =require("../Controllers/usuarios")
 const router=Router()
 router.get("/",getUsers)
 router.get("/id/:id",getUserByID)
 router.post("/",addUser)
+router.put("/",updateUserByUsuario)
 router.delete("/",deleteUserByID)
 
 module.exports=router
